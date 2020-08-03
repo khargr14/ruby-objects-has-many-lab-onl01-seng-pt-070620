130f8fe270246949c01ab2929952f3ecd73445ee
Keesha Hargrove <kharg14@gmail.com>
2020-08-03 23:50:36 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,20 @@
 class Post
-  attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
-  def author_name
-    @author ? @author.name : nil
-  end
+    attr_accessor :author, :title, :name
+
+    @@all = [] 
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+    def author_name
+        author.name if author
+    end
+
 end
