f4fd02dd827ec62d235a9eed29d23f15d15620dd
Keesha Hargrove <kharg14@gmail.com>
2020-08-03 23:54:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,27 @@
 class Artist
-    attr_accessor :name, :songs
-    @@all = []
-    def initialize(name)
-        @name = name
-        @songs = []
-        @@all << self
-    end
 
-    def add_song_by_name(name)
-        song = Song.new(name)
-        @songs << song
-        song.artist = self
-    end
+  attr_reader :name
 
-    def add_song(song)
-        @songs << song
-        song.artist = self
-    end
+  def initialize(name)
+    @name = name
+  end
 
-    def self.song_count
-        total = 0
-        @@all.each do |artist|
-            total += artist.songs.size
-        end
-        total
+  def songs
+    Song.all.select do |song|
+      song.artist == self
     end
+  end
 
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    new_song = Song.new(name)
+    new_song.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
 end
