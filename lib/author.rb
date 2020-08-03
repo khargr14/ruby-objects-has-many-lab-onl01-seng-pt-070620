a0f10c170a7280016975ac082b50ce7532bfe7dd
Keesha Hargrove <kharg14@gmail.com>
2020-08-03 23:46:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,5 @@
 class Author
-  attr_accessor :name
-
+  attr_accessor :name, :posts
   @@post_count = 0
 
   def initialize(name)
@@ -8,24 +7,18 @@ class Author
     @posts = []
   end
 
-  def posts
-    @posts
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
   end
 
   def self.post_count
     @@post_count
-  end
-
-  def add_post(post_title)
-    @posts << post_title
-    post_title.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
   end
 end
